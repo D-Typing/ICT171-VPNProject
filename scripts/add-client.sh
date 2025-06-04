585ba215d4e642cb033000d54b4f995b397ff17c
Declan.H <declangithub@gmail.com>
2025-06-04 20:17:20 +0800

    Fix adding clients and downloading files

--- scripts/add-client.sh
+++ scripts/add-client.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+
 USERNAME=$1
-cd /etc/openvpn/easy-rsa
-./easyrsa build-client-full $USERNAME nopass
-cp /etc/openvpn/client-template.txt /home/ubuntu/users/$USERNAME.ovpn
-cat pki/ca.crt >> /home/ubuntu/users/$USERNAME.ovpn
-cat pki/issued/$USERNAME.crt >> /home/ubuntu/users/$USERNAME.ovpn
-cat pki/private/$USERNAME.key >> /home/ubuntu/users/$USERNAME.ovpn
+cd /etc/openvpn/easy-rsa || exit 1
+
+if [[ -f "pki/issued/$USERNAME.crt" ]]; then
+    echo "[!] Certificate for user '$USERNAME' already exists. Overwriting..."
+    echo yes | ./easyrsa --batch build-client-full "$USERNAME" nopass
+else
+    ./easyrsa --batch build-client-full "$USERNAME" nopass
+fi
+
+OVPN_FILE="/home/ubuntu/users/$USERNAME.ovpn"
+cp /etc/openvpn/client-template.txt "$OVPN_FILE"
+cat pki/ca.crt >> "$OVPN_FILE"
+cat pki/issued/$USERNAME.crt >> "$OVPN_FILE"
+cat pki/private/$USERNAME.key >> "$OVPN_FILE"
+
+echo "[+] Configuration for '$USERNAME' created at $OVPN_FILE"
